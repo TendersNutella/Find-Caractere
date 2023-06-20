504d4f19599f0649af59ec2ebdf4afc94b06061c
TendersNutella <129865316+TendersNutella@users.noreply.github.com>
2023-06-20 16:44:13 +0200

    Update Mot.cpp

--- Mot.cpp
+++ Mot.cpp
@@ -7,6 +7,24 @@ void Mot::begin()
 	std::cout << "Le premier caractere est : " << caractere << std::endl;
 }
 
+void Mot::middle()
+{
+  	int nCaractere = mot.size() - 1;
+
+	if (nCaractere % 2 == 0)
+	{
+		nCaractere = nCaractere / 2;
+
+		char caractere = mot[nCaractere];
+
+		std::cout << "Le caractere du millieu est : " << caractere << std::endl;
+	}
+	else
+	{
+		std::cout << "ERREUR : Le nombre de caractere est pair." << std::endl;
+	}
+}
+
 void Mot::end()
 {
 	int const nCaractere = mot.size();
